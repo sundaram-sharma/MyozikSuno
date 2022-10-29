aefbced6e2f10cd564887a07035bd00219454022
Sundaram Sharma <sundaram-sharma@hotmail.com>
2022-10-29 22:42:24 +0530

    Initial Commit .Added Permission .Started Working on the UI

---
+++ app/src/main/java/en/sundaram/myoziksuno/MainActivity.java
@@ -0,0 +1,58 @@
+package en.sundaram.myoziksuno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    String[] items;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.listView);
+        runtimePermission();
+
+    }
+
+    public void runtimePermission()
+    {
+        Dexter.withContext(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+
+                    //methods will be required depending on the scenario
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+                    }
+                }).check();
+
+    }
+}
